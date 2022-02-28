70eafafa3cf2373dd91a580406a794b7873c913b
Pedro Bonfilio <pedroblimaa@gmail.com>
2022-02-28 20:48:09 -0300

    Adding initial list of games

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
 const router = require("express").Router()
 
-const IgdbController = require("./controllers/IgdbController")
 const GamesController = require("./controllers/GamesController")
 
-router.get("/getToken", IgdbController.getToken)
 router.get("/getGames", GamesController.getGames)
 
 module.exports = router
---
+++ src/services/IgdbService.js
@@ -0,0 +1,41 @@
+const axios = require("axios")
+require("dotenv").config()
+
+const host = "https://api.igdb.com/v4"
+
+module.exports = {
+  getIgdbToken: async () => {
+    const requestPath =
+      "/oauth2/token?client_id=" +
+      process.env.IGDB_CLIENT_ID +
+      "&client_secret=" +
+      process.env.IGDB_SECRET +
+      "&grant_type=client_credentials"
+
+    const igdbCredentialsResponse = await axios.post(
+      "https://id.twitch.tv" + requestPath
+    )
+    return igdbCredentialsResponse.data.access_token
+  },
+
+  getGames: async (igdbToken, limit = 12, offset = 0) => {
+    const fields =
+      "id,category,external_games,game_modes,genres,platforms,release_dates,screenshots,similar_games,slug,summary"
+
+    const requestPath =
+      host + "/games?fields=" + fields + "&limit=" + limit + "&offset=" + offset
+
+    const games = await axios.get(
+      requestPath + "&order=release_dates.date%3Adesc&search=",
+      {
+        headers: {
+          Authorization: "Bearer " + igdbToken,
+          Accept: "application/json",
+          "Client-ID": process.env.IGDB_CLIENT_ID,
+        },
+      }
+    )
+
+    return games.data
+  },
+}
